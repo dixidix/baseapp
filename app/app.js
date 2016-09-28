0df5f6c0d17ba52b523e29063674bc51eea73c56
nicolas sigal <nicolas.sigal@globallogic.com>
2016-09-28 12:53:49 -0300

    auth

--- app/app.js
+++ app/app.js
@@ -4,11 +4,16 @@
 		.config(function ($stateProvider, $urlRouterProvider) {
 			$urlRouterProvider.otherwise("/");
 			$stateProvider
-				.state('home', { url: "/", templateUrl: "./dist/routes/home/home.template.html", data: { title: 'Home' }, controller: "homeCtrl", controllerAs: "home" })
-		});
-		app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
-				$rootScope.$state = $state;
-				$rootScope.$stateParams = $stateParams;
-			}]);
+				.state('home', { url: "/", templateUrl: "./dist/routes/home/home.template.html", data: { title: 'Home', requireAuth: false }, controller: "homeCtrl", controllerAs: "home" });
+		})
+		.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+			$rootScope.$state = $state;
+			$rootScope.$stateParams = $stateParams;
+			$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+				if (toState.data.requireAuth) {
+					$state.go('home');
+				}
+			});
+		}]);
 	require('./routes/home/home.js')(angular, app);
 })();
